24f1efd877f7150c7d140aba912c481a617fcbb9
Mabel <macmota@hotmail.com>
2018-10-03 09:06:34 -0400

    applied normalize.css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+//import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
-//import './index.css';
+// ----- styles/index.js
+import styledNormalize from "styled-normalize";
+import { injectGlobal } from "styled-components";
+
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+
+injectGlobal`
+  ${styledNormalize}
+
+  // You can continue writing global styles
+  body {
+   
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
